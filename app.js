2d26b5284350a030374e13cc7515c5d963d957b6
Adrien Auxent <a.auxent@gmail.com>
2022-01-23 16:48:12 +0100

    wow

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 let body = document.querySelector("body");
-var BAM = document.createElement("div");
-var newContent = document.createTextNode('BAM!');
-BAM.appendChild(newContent);
-BAM.classList.add("BAM");
+var BAM = document.querySelector(".BAM");
+var hero = document.querySelector(".hero");
+var container = document.querySelector(".container");
 
-body.addEventListener("mousedown", e => {
-  // et lui donne un peu de contenu
-  // ajoute le nœud texte au nouveau div créé
-  
-  // ajoute le nouvel élément créé et son contenu dans le DOM
-  var container = document.getElementById('container');
+setInterval(bam, 500, 'Parameter 1', 'Parameter 2');
+
+function bam() {
+  console.log("ok")
   var randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
   var randomColor2 = "#"+((1<<24)*Math.random()|0).toString(16); 
-  BAM.style.setProperty('background-color', randomColor);
+  container.style.setProperty('background-color', randomColor);
   BAM.style.setProperty('color', randomColor2);
-  document.body.insertBefore(BAM, container);
-
-//   BAM.style.left = `${e.clientX.toString()}px`;
-//   BAM.style.top = `${(e.clientY.toString() - 20 )}px`;
-  setTimeout(() => {  
-    //   BAM.remove();
-   }, 100);
-});
-
-body.addEventListener("mouseup", e => {
-    BAM.remove();
-})
+}
